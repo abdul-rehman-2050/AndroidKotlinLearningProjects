4cb4cc06b2607055b69de81bf18193cb2d12b0bb
abdul-rehman-2050 <supermind2002@gmail.com>
2017-10-20 00:33:58 +0500

    different color picker options added

--- ChangeColorEx01/app/src/main/java/ex01/change/color/learnkotlin/com/changecolorex01/MainActivity.kt
+++ ChangeColorEx01/app/src/main/java/ex01/change/color/learnkotlin/com/changecolorex01/MainActivity.kt
@@ -1,6 +1,6 @@
 package ex01.change.color.learnkotlin.com.changecolorex01
 
-import android.app.TaskStackBuilder.create
+
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -14,11 +14,24 @@ import me.priyesh.chroma.ColorSelectListener;
 
 
 
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 class MainActivity : AppCompatActivity() {
+
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val adapter = ArrayAdapter(
+                this, android.R.layout.simple_spinner_dropdown_item, ColorMode.values())
+        color_mode_spinner.setAdapter(adapter)
+        color_mode_spinner.setSelection(adapter.getPosition(ColorMode.RGB))
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -32,10 +45,11 @@ class MainActivity : AppCompatActivity() {
     private fun showColorPickerDialog() {
         ChromaDialog.Builder()
                 .initialColor(Color.BLUE)
-                .colorMode(ColorMode.RGB)
+                .colorMode(color_mode_spinner.selectedItem as ColorMode)
                 .onColorSelected(object : ColorSelectListener {
                     override fun onColorSelected(color: Int) {
-                       Toast.makeText(applicationContext,"Color is = "+color,Toast.LENGTH_SHORT).show()
+                        //#TODO make use of color to change textview foreground color
+                        Toast.makeText(applicationContext,"Color is = "+color,Toast.LENGTH_SHORT).show()
                     }
                 })
                 .create()
@@ -52,8 +66,13 @@ class MainActivity : AppCompatActivity() {
 
             }
             R.id.menu_item_quit ->{
-
                 return true
+
+              //#TODO implement onQuit confirmation dialog and exit application if user confirms
+
+                
+
+
 
             }
             else ->return super.onOptionsItemSelected(item)
